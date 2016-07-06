e8c1014e34f3a5b49ecd0c86769c80697399ba8a
JiananLuo <matthewluoo@gmail.com>
2016-07-06 14:41:39 -0400

    Add sys_execv (no arg)

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -12,6 +12,8 @@
 
 #include "opt-A2.h"
 #include <mips/trapframe.h>
+#include <vfs.h>
+#include <kern/fcntl.h>
 
   /* this implementation of sys__exit does not do anything with the exit code */
   /* this needs to be fixed to get exit() and waitpid() working properly */
@@ -165,8 +167,8 @@ sys_waitpid(pid_t pid,
 }
 
 #if OPT_A2
-int
-sys_fork(struct trapframe * parent_trapframe, pid_t * retval)
+
+int sys_fork(struct trapframe * parent_trapframe, pid_t * retval)
 {
   int result;
   struct proc *cp = curproc;
@@ -206,4 +208,63 @@ sys_fork(struct trapframe * parent_trapframe, pid_t * retval)
   *retval = child_process->pid;
   return 0;
 }
+
+int sys_execv(userptr_t progname, userptr_t args)
+{
+  char * name;
+  name = (char *)progname;
+  char ** arguments;
+  arguments = (char **)args;
+
+  struct addrspace *as;
+	struct vnode *v;
+	vaddr_t entrypoint, stackptr;
+	int result;
+
+	/* Open the file. */
+	result = vfs_open(name, O_RDONLY, 0, &v);
+	if (result) {
+		return result;
+	}
+
+	/* Create a new address space. */
+  as = curproc_getas();
+  as_destroy(as);
+	as = as_create();
+	if (as ==NULL) {
+		vfs_close(v);
+		return ENOMEM;
+	}
+
+	/* Switch to it and activate it. */
+	curproc_setas(as);
+	as_activate();
+
+	/* Load the executable. */
+	result = load_elf(v, &entrypoint);
+	if (result) {
+		/* p_addrspace will go away when curproc is destroyed */
+		vfs_close(v);
+		return result;
+	}
+
+	/* Done with the file now. */
+	vfs_close(v);
+
+	/* Define the user stack in the address space */
+	result = as_define_stack(as, &stackptr);
+	if (result) {
+		/* p_addrspace will go away when curproc is destroyed */
+		return result;
+	}
+
+	/* Warp to user mode. */
+	enter_new_process(0 /*argc*/, NULL /*userspace addr of argv*/,
+			  stackptr, entrypoint);
+
+	/* enter_new_process does not return. */
+	panic("enter_new_process returned\n");
+	return EINVAL;
+}
+
 #endif
